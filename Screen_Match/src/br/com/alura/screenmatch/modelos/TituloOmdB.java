2aab670b517e2e98443b2e330c97828733946681
Celso Bitello <98421039+Celso1404@users.noreply.github.com>
2024-11-09 20:47:02 -0300

    Add files via upload

---
+++ Screen_Match/src/br/com/alura/screenmatch/modelos/TituloOmdB.java
@@ -0,0 +1,4 @@
+package br.com.alura.screenmatch.modelos;
+
+public record TituloOmdB(String title, String year, String runtime) {
+}
---
+++ Screen_Match/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -0,0 +1,38 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.modelos.Titulo;
+import br.com.alura.screenmatch.modelos.TituloOmdB;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalComBusca {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite um filme para busca: ");
+        var busca = leitura.nextLine();
+
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=c60636d0";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        String json = response.body();
+        System.out.println(json);
+
+        Gson gson = new Gson();
+        //Titulo meutititulo = gson.fromJson(json, Titulo.class);
+        TituloOmdB meuTituloOmdB = gson.fromJson(json, TituloOmdB.class);
+        System.out.println(meuTituloOmdB);
+
+    }
+}
